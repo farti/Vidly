59fc9544a56b0758d469ee57d057132e7ec2891a
Marcin Rakoczy <mrakoczy@wp.pl>
2018-07-02 11:50:43 +0200

    customer edit form

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -23,11 +23,11 @@ namespace Vidly.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var vieModel = new NewCustomerViewModel
+            var vieModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershipTypes
             };
-            return View(vieModel);
+            return View("CustomerForm",vieModel);
         }
 
         [HttpPost]
@@ -55,6 +55,25 @@ namespace Vidly.Controllers
             }
 
             return View(customer);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
+
         }
     }
 }
