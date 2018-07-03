b2f730d03a4a0e3cd4aa412ad0eb0914c45ec83f
Marcin Rakoczy <mrakoczy@wp.pl>
2018-07-03 08:23:42 +0200

    Movie ValidateAntiForgeryToken

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -32,6 +32,7 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
             if (!ModelState.IsValid)
