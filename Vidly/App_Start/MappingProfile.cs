7911c8ae3fbafa32a7aad7ffe2feacedf48d21a0
Marcin Rakoczy <mrakoczy@wp.pl>
2018-07-04 14:40:26 +0200

    fixed membershipType

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -8,13 +8,17 @@ namespace Vidly.App_Start
     {
         public MappingProfile()
         {
-            Mapper.CreateMap<Customer, CustomerDto>()
-                .ForMember(c=>c.Id, opt => opt.Ignore());
-            Mapper.CreateMap<CustomerDto, Customer>();
+            // Domain to Dto
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MembershipType, MembershipTypeDto>();
 
-            Mapper.CreateMap<Movie, MovieDto>()
+            // Dto to Domain
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+            Mapper.CreateMap<MovieDto, Movie>()
                 .ForMember(m=>m.Id, opt => opt.Ignore());
-            Mapper.CreateMap<MovieDto, Movie>();
+            
 
         }
     }
